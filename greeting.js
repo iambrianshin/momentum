7ff9d0c2491ce4a3aa232f74628459f1cab819c8
Brian Shin <39751822+iambrianshin@users.noreply.github.com>
2019-02-12 22:26:49 -0500

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,34 +1,51 @@
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
+/*
+|------------------------------------------
+| Note.
+|------------------------------------------
+|
+| - localstorage : 유저컴퓨터(브라우저)에 정보를 저장하고있는 스토리지.
+| - classList : 해당 엘리먼트의 class를 컨트롤 할 수 있다.
+| - addEventListener : 해당 이벤트를 캐치한다.
+| - event.preventDefault : 이벤트의 기본값을 없앤다.
+|
+*/
+
+const greetingForm = document.querySelector(".js-greetingForm"),
+  greetingInput = greetingForm.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "curerntUser",
-  SHOWING_CN = "showing";
+const USER_LOCALSTORAGE = "curerntUser",
+  SHOWING_CLASSNAME = "showing",
+  HIDING_CLASSNAME = "hiding";
 
 function saveName(text) {
-  localStorage.setItem(USER_LS, text);
+  // 로컬스토리지에 저장하면 브라우저가 refresh 되어도 값을 가지고 있다.
+  localStorage.setItem(USER_LOCALSTORAGE, text);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = input.value;
+  const currentValue = greetingInput.value;
+  greetingForm.classList.add(HIDING_CLASSNAME);
+  toDoForm.classList.remove(HIDING_CLASSNAME);
   paintGreeting(currentValue);
   saveName(currentValue);
+  document.querySelector(".js-toDoInput").focus();
 }
 
 function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
+  greetingInput.focus();
+  greetingForm.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+  greetingForm.classList.add(HIDING_CLASSNAME);
+  greeting.classList.add(SHOWING_CLASSNAME);
+  greeting.innerText = `Hello ${text}. What is your to do for today?`;
 }
 
 function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
+  const currentUser = localStorage.getItem(USER_LOCALSTORAGE);
   if (currentUser === null) {
     askForName();
   } else {
